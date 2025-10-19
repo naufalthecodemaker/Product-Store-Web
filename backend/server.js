5cf235a51071de8fd1acddb47374d5b42dd52766
naufalthecodemaker <naufalrahman867@gmail.com>
2025-10-19 09:54:13 +0700

    feat: add create product endpoint in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.post("/products", async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if(!product.name || !product.price || !product.image){
+    return res.status(400).json({success:false, message: "Please provide all fields!"});
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({success:true, data: newProduct});
+  } catch (error) {
+    console.error("Error in create product: ", error.message);
+    res.status(500).json({success:false, message: "Server error!"});
+  }
+});
 
 app.listen(5000, () => {
   connectDB();
