f7afa57bee81cd251ca10d53676f9f02efc4251a
naufalthecodemaker <naufalrahman867@gmail.com>
2025-10-19 11:21:16 +0700

    feat: add delete product endpoint in server.js

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,22 @@ app.post("/api/products", async (req, res) => {
   const newProduct = new Product(product);
 
   try {
-    await newProduct.save();
+    await newProduct.save(); // MongoDB automatically makes an ID for new product
     res.status(201).json({success:true, data: newProduct});
   } catch (error) {
     console.error("Error in create product: ", error.message);
     res.status(500).json({success:false, message: "Server error!"});
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const {id} = req.params;
+  
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({success:true, message: "Product deleted!"});
+  } catch (error) {
+    res.status(404).json({success:false, message: "Product not found!"});
   }
 });
 
